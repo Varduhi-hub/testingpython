ccf10be926d3ad3c5261d4047bc666ec7c050dff
Varduhi-hub <57668735+Varduhi-hub@users.noreply.github.com>
2019-12-12 01:15:59 +0400

    Add files via upload

---
+++ learningmaterial.py
@@ -0,0 +1,24 @@
+# using center(width[fillchar]) function for aligning center the variable
+user_type = "Student user"
+print(user_type.center(50))
+
+# using replace(function) for modifying the value
+print(user_type.replace("Student user", "Teacher user"))
+
+# using capitalize function for replacing the 1st string of the variables into capital letter
+name = "varduhi"
+surname = "Galstyan"
+print(name.capitalize() + " " + surname.capitalize())
+
+# using isalnum() function for verifying that "password" variable has at least 1 character
+password = "Vgalstyan1"
+print(password.isalnum())
+
+
+
+
+
+
+
+
+
